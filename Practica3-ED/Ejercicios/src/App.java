53a5ed8ffc1d34fc4a94a18bdfe4431be3a6789f
SoyLeooo <161857861+SoyLeooo@users.noreply.github.com>
2024-03-04 18:40:07 +0100

    Update App.java

--- Practica3-ED/Ejercicios/src/App.java
+++ Practica3-ED/Ejercicios/src/App.java
@@ -1,17 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
                 //Declaramos las variables
-                int num1=10;
-                int num2=5;
+                int num1=13;
+                int num2=6;
         
                 /*Realizamos las operaciones.
                  * Tambien lo podemos guardar el resultado en variables. */
         
         
+                System.out.println("El resultado de la suma es "+(num1+num2));
+                
                 System.out.println("El resultado de la resta es "+(num1-num2));
 
-                System.out.println("El resultado de la suma es "+(num1+num2));
-        
                 System.out.println("El resultado de la multiplicación es "+(num1*num2));
         
                 System.out.println("El resultado de la división es "+(num1/num2));
